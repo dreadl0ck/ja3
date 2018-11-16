fe2a9d69651b47e45913d685698bc5d1d106f336
Philipp Mieden <dreadl0ck@protonmail.ch>
2018-11-16 23:27:51 +0100

    comments

--- json.go
+++ json.go
@@ -1,3 +1,16 @@
+/*
+ * JA3 - TLS Client Hello Hash
+ * Copyright (c) 2018 Philipp Mieden <dreadl0ck [at] protonmail [dot] ch>
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 package ja3
 
 import (
@@ -64,14 +77,17 @@ func ReadFileJSON(file string, out io.Writer) {
 				tl = p.TransportLayer()
 			)
 
+			// stop if either network or transport layer is nil
 			if tl == nil || nl == nil {
 				fmt.Println("error: ", nl, tl, p.Dump())
 				continue
 			}
 
+			// convert ports to integers, ignore errors
 			srcPort, _ := strconv.Atoi(tl.TransportFlow().Src().String())
 			dstPort, _ := strconv.Atoi(tl.TransportFlow().Dst().String())
 
+			// append record and populate all fields
 			records = append(records, &Record{
 				DestinationIP:   nl.NetworkFlow().Dst().String(),
 				DestinationPort: dstPort,
@@ -84,17 +100,20 @@ func ReadFileJSON(file string, out io.Writer) {
 		}
 	}
 
+	// make it pretty please
 	b, err := json.MarshalIndent(records, "", "    ")
 	if err != nil {
 		panic(err)
 	}
 
+	// write to output io.Writer
 	_, err = out.Write(b)
 	if err != nil {
 		panic(err)
 	}
 }
 
+// convert a time.Time to a string timestamp in the format seconds.microseconds
 func timeToString(t time.Time) string {
 	micro := fmt.Sprintf("%06d", t.Nanosecond()/1000)
 	return strconv.FormatInt(t.Unix(), 10) + "." + micro
